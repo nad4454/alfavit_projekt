645ddee145f408f2cc6bbf5f782d8a4e0c7c7f7f
Nadezhda S <nad4454@yandex.ru>
2022-10-17 23:39:07 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "alfavit_projekt"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = listOf(1, 9, 47, 65, 88)
+    var sum = 0
+    for (element in list){
+        sum += element
+    }
+
+    println(sum)
+}
